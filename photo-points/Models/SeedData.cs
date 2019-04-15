8a12399bb20f42b5a5b2589e3465684a72c4f001
Eric Ro <ericro914@gmail.com>
2019-04-15 13:05:01 -0700

    Created seed data

---
+++ photo-points/Models/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+
+namespace photo_points.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices
+            .GetRequiredService<ApplicationDbContext>();
+            if (!context.Submissions.Any())
+            {
+                context.Submissions.AddRange(
+                    new Submission { User = "Eric", ApprovalStatus = false },
+			        new Submission { User = "Ro", ApprovalStatus = true },
+			        new Submission { User = "BIT286", ApprovalStatus = false },
+			        new Submission { User = "PhotoPoints", ApprovalStatus = true }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- photo-points/Startup.cs
+++ photo-points/Startup.cs
@@ -62,6 +62,7 @@ namespace photo_points
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
