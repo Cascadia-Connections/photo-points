af763c7f25f6533e36319cf9e1bf82def493039b
Eric Ro <ericro914@gmail.com>
2019-04-15 12:24:35 -0700

    Created database classes and repository class

---
+++ photo-points/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace photo_points.Models
+{
+    public class ApplicationDbContext : DbContext
+{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        public DbSet<Submission> Submissions { get; set; }
+}
+}
