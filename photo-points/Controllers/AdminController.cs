72edcd03f6419309ea8b24f27c2e56b1f5993d41
sara1983 <Sarah.ansari246@gmail.com>
2019-04-15 13:10:15 -0700

    done issue#6

--- photo-points/Controllers/AdminController.cs
+++ photo-points/Controllers/AdminController.cs
@@ -41,7 +41,7 @@ namespace photopoints.Controllers
         public IActionResult Update(long id)
         {
             User user = _pdc.Users.Single(u => u.UserId == id);
-            return View("", user);
+            return View("WelcomAdmin", user);
         }
     }
 }
