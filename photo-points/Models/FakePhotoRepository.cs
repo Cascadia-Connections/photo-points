565fe082d957819474765bbef74dfdee3d81a77a
sara1983 <Sarah.ansari246@gmail.com>
2019-04-29 12:20:12 -0700

    issue #13

---
+++ photo-points/Models/FakePhotoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace photo_points.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class FakePhotoRepository : IProductRepository
+    {
+        public IQueryable<Photo> photos => new List<Photo> {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surf board", Price = 179 },
+            new Product { Name = "Running shoes", Price = 95 }
+        }.AsQueryable<Photo>();
+    }
