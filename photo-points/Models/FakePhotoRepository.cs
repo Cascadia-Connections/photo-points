93ca3f3e3b17cd075669d557f62eb7f289d90598
sara1983 <Sarah.ansari246@gmail.com>
2019-04-29 13:06:11 -0700

    isuue #13

--- photo-points/Models/FakePhotoRepository.cs
+++ photo-points/Models/FakePhotoRepository.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace photo_points.Models
 {
     // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
-    public class FakePhotoRepository : IProductRepository
+    public class FakePhotoRepository : 
     {
-        public IQueryable<Photo> photos => new List<Photo> {
-            new Product { Name = "Football", Price = 25 },
-            new Product { Name = "Surf board", Price = 179 },
-            new Product { Name = "Running shoes", Price = 95 }
-        }.AsQueryable<Photo>();
+        public IQueryable<Capture> captures => new List<Capture> {
+            new Capture { photo = 240, captureDate = 4/25/2019 },
+            new Capture { photo = 240, captureDate = 3/13/2019 },
+            new Capture { photo = 240, captureDate = 2/3/2019 }
+        }.AsQueryable<Capture>();
+
+
+        public IQueryable<User> users => new List<User> {
+            new User { firstName = "Sara", lastName = "Ansari" },
+            new User { firstName = "Sim", lastName = "Ruble" },
+            new User { firstName = "Willie", lastName = "Weber" }
+        }.AsQueryable<User>();
     }
+}
