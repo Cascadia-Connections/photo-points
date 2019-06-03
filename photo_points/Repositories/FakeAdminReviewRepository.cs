8d4dc10512069e44f95664df939351f35eceebf0
sara1983 <Sarah.ansari246@gmail.com>
2019-06-03 11:33:17 -0700

    adding code to save changes

--- photo_points/Repositories/FakeAdminReviewRepository.cs
+++ photo_points/Repositories/FakeAdminReviewRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using photo_points.Models;
 
@@ -73,5 +74,12 @@ namespace photo_points.Repositories
          return captures.Where(a => a.approval == Capture.ApprovalType.Approve);
 
         }
+
+        public IQueryable<Capture> SavedCaptures()
+        {
+            return captures;
+        }
+
+      
     }
 }
--- photo_points/Repositories/IAdminReviewRepository.cs
+++ photo_points/Repositories/IAdminReviewRepository.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using photo_points.Models;
 
-namespace photo_points.Models
+namespace photo_points.Repositories
 {
     public interface IAdminReviewRepository
     {
         IQueryable<Capture> GetCaptures();
         Capture GetCapture(long captureId);
         IEnumerable<Capture> GetAllUnapproved();
+
+        IQueryable<Capture> SavedCaptures();
+
+
     }
 }
