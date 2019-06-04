b818a567e9a538439bf8642460a15904d17cbc3b
sruble <sim.ruble@gmail.com>
2019-06-03 22:08:01 -0700

    about to start foreach loop

--- photo_points/Controllers/AdminController.cs
+++ photo_points/Controllers/AdminController.cs
@@ -6,9 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using photo_points.Models;
+using photo_points.ViewModels;
 using photo_points.Controllers;
 using photo_points.Services; // added from issue #47
-using static System.Net.Mime.MediaTypeNames;
+using System.IO;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,15 +18,22 @@ namespace photo_points.Controllers
     public class AdminController : Controller
     {
    
-        private IAdminReviewServices _pdc;
+        private IAdminReviewServices _adminReviewServices;
 
-        public AdminController(IAdminReviewServices pdContext)
+        public AdminController(IAdminReviewServices adminServiceReview)
 
         {
-            _pdc = pdContext;
+            _adminReviewServices = adminServiceReview;
         }
 
         ///UNComment above block of code before commit
+        ///
+        // private Image byteArrayToImage(byte[] byteArrayIn)
+        //{
+        //    MemoryStream ms = new MemoryStream(byteArrayIn);
+        //    Image returnImage = Image.FromStream(ms);
+        //    return returnImage;
+        //}
 
 
         // GET: /<controller>/
@@ -51,10 +59,45 @@ namespace photo_points.Controllers
         [HttpGet]
         public IActionResult Pending()
         {
-            return View("Pending");
+            //build a view model 
+
+            PendingViewModel pvm = new PendingViewModel();
+
+            _adminReviewServices.GetUnapprovedCaptures(); 
+
+            //create a foreach loop that goes thru the list and pulls out images that have NOT been approved. 
+           
+
+            //if()
+            //{
+                return View("Pending");
+
+            //}
+
+
+            //return View("Pending");
             //all the new submissions can be displayed
             // //  if Capture approval is false. 
             // // display the Capture image and Data. 
+
+
+
+            //foreach(photo_points.Models.Capture approve in @Models)
+            //    {
+            //    if (approve == false)
+            //    {
+            //        //Card list html will go here
+
+            // // Below 3 lines of code are from Brian 
+
+                    //string mimeType = /* Get mime type somehow (e.g. "image/png") */;
+                    //string base64 = Convert.ToBase64String(yourImageBytes);
+                    //return string.Format("data:{0};base64,{1}", mimeType, base64); 
+
+            //    }
+
+            //}
+            //foreach (approved in IEnumerable) 
 
         }
 
---
+++ photo_points/ViewModels/PendingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace photo_points.ViewModels
+{
+    public class PendingViewModel
+    {
+
+        //carry from capture//convert and carry to the View
+
+        public string ImageSource { get; set; }
+      
+    }
+}
