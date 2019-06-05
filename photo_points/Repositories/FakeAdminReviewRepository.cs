eea62537a1a4c300c841b33b80efce17de0cd33b
Eric Ro <erictro@student.cascadia.edu>
2019-06-05 12:12:18 -0700

    Correcting file reference

--- photo_points/Repositories/FakeAdminReviewRepository.cs
+++ photo_points/Repositories/FakeAdminReviewRepository.cs
@@ -24,11 +24,11 @@ namespace photo_points.Repositories
             throw new NotImplementedException();
         }
 
-        byte[] imgdata = System.IO.File.ReadAllBytes("/wwwroot/images/maple-leaf-888807_640.jpg");
+        byte[] imgdata = System.IO.File.ReadAllBytes("wwwroot/images/maple-leaf-888807_640.jpg");
 
-        byte[] imgdata1 = System.IO.File.ReadAllBytes("/wwwroot/images/blackberry-flower-4070045_640.jpg");
+        byte[] imgdata1 = System.IO.File.ReadAllBytes("wwwroot/images/blackberry-flower-4070045_640.jpg");
 
-        byte[] imgdata2 = System.IO.File.ReadAllBytes ("/wwwroot/images/fern-1105988_640.jpg");
+        byte[] imgdata2 = System.IO.File.ReadAllBytes ("wwwroot/images/fern-1105988_640.jpg");
 
         // Need to pull Willie's updated entity model with "Approved" property
         public IQueryable<Capture> captures => new List<Capture> {
