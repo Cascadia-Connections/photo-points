ebcdd29fa9f8dd68668cf5a2d82fd52c53d85bd3
sruble <sim.ruble@gmail.com>
2019-06-10 12:21:00 -0700

    changing branchses

--- photo_points/Controllers/AdminController.cs
+++ photo_points/Controllers/AdminController.cs
@@ -60,25 +60,22 @@ namespace photo_points.Controllers
         public IActionResult Pending()
         {
             //build a view model //
-             PendingViewModel pvm = new PendingViewModel(); //// or below line of code
-            //List<PendingViewModel> pvm = new List<PendingViewModel>();
-
+             PendingViewModel pvm = new PendingViewModel();
+            
+            //start with entire collection
             IEnumerable<Capture> pendingCaptures = _adminReviewServices.GetUnapprovedCaptures();
 
-            //create a foreach loop that goes thru the list and pulls out images that have NOT been approved. 
+            //create a foreach loop that goes thru the list and converts bytes to string. 
             foreach (Capture capture in pendingCaptures) // looking at comments on line 18 and 23 in Capture.cs // should we display all images since they are the default?
             {
                 string mimeType = "images/jpeg";
                 string base64 = Convert.ToBase64String(capture.photo); ////
-                 //string.Format("fate:{0}; base64,{1}", mimeType, base64);
-
+                string.Format("fate:{0}; base64,{1}", mimeType, base64);
                 pvm.ImageSource.Add(string.Format("fate:{0}; base64,{1}", mimeType, base64));
-                
             }
             return View("Pending", pvm);
-            //return View("Pending");
         }
-        // // whould this be followed by a GetEnumerator?
+
 
         [HttpGet]
         public IActionResult SearchPhotoPoints()
