c19184410d844207716b0a7e9d55733b3740bf84
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2020-05-03 18:19:56 -0700

    Able to add and delete notes from the details picture. But it doens't store it to the database. This was done using js.

---
+++ photo_points/wwwroot/js/addNotes.js
@@ -0,0 +1,12 @@
+﻿function AddNote() {
+   var userInput = "";
+   var output = "";
+   userInput = prompt("Type your notes here");
+   output += userInput;
+   document.getElementById('outputmsg').innerHTML = output;
+}
+
+function DeleteNote() {
+   var output = "";
+   document.getElementById('outputmsg').innerHTML = output;
+}
