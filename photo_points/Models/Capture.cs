d553139ac712e61de69ad70d95ced7dab1fbed08
gjesionowski <gregory.a.jesionowski@gmail.com>
2020-06-01 12:14:49 -0700

    Updated with latest version of develop after receiving final approval

--- photo_points/Models/Capture.cs
+++ photo_points/Models/Capture.cs
@@ -20,7 +20,7 @@ namespace photo_points.Models
 
     public enum ApprovalType
         {
-            //Pending is the default. So when a new capture is created it will be waiting for admin approval.
+            //Pending is the default. So when a new capture is created it will be waiting for admin approval
             Pending,
             Approve, 
             Reject  
