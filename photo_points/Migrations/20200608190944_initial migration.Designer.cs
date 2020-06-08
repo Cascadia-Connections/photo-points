5b6506878cd5c29535bd4f9fe093bcae5419d218
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2020-06-08 12:12:15 -0700

    The add notes feature should work with the new develop branch without any conflicts

--- photo_points/Migrations/20200608190944_initial migration.Designer.cs
+++ photo_points/Migrations/20200608190944_initial migration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using photo_points.Models;
 
 namespace photo_points.Migrations
 {
     [DbContext(typeof(PhotoDataContext))]
-    partial class PhotoDataContextModelSnapshot : ModelSnapshot
+    [Migration("20200608190944_initial migration")]
+    partial class initialmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
